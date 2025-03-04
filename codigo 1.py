5e299fd7df75fe7c47d3335be22d03e145ff675f
Angel Jesus Libreros Ponce <angelibpo@outlook.com>
2025-03-03 22:03:43 -0600

    Add files via upload

---
+++ codigo 1.py
@@ -0,0 +1,20 @@
+# Hola esto es una prueba
+
+# Hola esto lo estoy escribiendo desde mi PC
+numero1 = 5
+numero2 = 3
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+
+# validación para evitar división por cero
+if numero2 == 0:
+    print("Error: División por cero no es permitida.")
+else:
+    division = numero1 / numero2
+    print("División:", division)
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
