112d7bb09b763dc7bd355b3d6d8c6f96c61ca034
Ferriv0104 <zS23013928@estudiantes.uv.mx>
2025-03-03 22:26:06 -0600

    Update Ejemplo 2 - F01.py
    Puse cometarios dentro del código

--- Ejemplo 2 - F01.py
+++ Ejemplo 2 - F01.py
@@ -1,11 +1,17 @@
-frutas = ["manzana", "plátano", "naranja"]
+#Equipo1 
+#Trabajo Maria Fernanda Rivera Vega 
+#03 de Marzo del 2025 
+#Este codigo nos imprira una lista de frutas que vayamos agregando 
+
+frutas = ["manzana", "plátano", "naranja"] #Definimos una lista llamda frutas que contiene los elementos manzana, platano y naranja. 
 
 
-frutas.append("pera")
+frutas.append("pera") #El comando append agraga datos a la lista, en este caso pera 
 
 
-print("Lista de frutas:", frutas)
+print("Lista de frutas:", frutas) #Esta linea nos manda a imprimir la frase Lista de frutas y la fruta.
 
 
-for fruta in frutas:
-    print("Tengo una", fruta)
+for fruta in frutas: #empezamos un bucle para recorrer un elemento
+    print("Tengo una", fruta) #Nos imprime el texto Tengo una mas la fruta 
+    #El bucle aun se imprime cuando la lista se modifique 
